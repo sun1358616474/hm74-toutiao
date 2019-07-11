f92c9e477b4cfe49941fdd840b8c8e7347781876
sun1358616474 <1358616474@qq.com>
2019-07-11 21:31:14 +0800

    完成页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 导入ui框架
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 导入vue-router
 import router from '@/router'
+// 导入axios
+import axios from 'axios'
+Vue.prototype.axios = axios
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,12 +2,80 @@
   <div class="login-container">
     <el-card class="login-box">
       <img src="../../assets/images/logo_index.png" alt />
+      <!-- 登录表单 -->
+      <el-form ref="loginForm" :status-icon="true" :model="loginForm" :rules="loginRules">
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:240px"></el-input>
+          <el-button style="float:right" type="primary">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value='true'>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%" type="primary" @click="login()">登  录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    // 效验手机号
+    const checkMobile = (rule, value, callback) => {
+      // 效验逻辑   把value拿出来进行手机号的格式效验
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不正确'))
+      }
+    }
+    return {
+      // 表单数据对象
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      // 表单验证规则对象
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '请输入6位数字', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      // 对整个表单进行效验
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          // 提交登录请求 axios是基于primise封装的post（）返回值一个promise对象
+          this.axios
+            .post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then(res => {
+              // res是响应对象 包含 后台返回的数据 res.data
+              // 1 跳转到首页
+              // 2 保存用户的信息 用来判断登录的状态  未完成
+              this.$router.push('/')
+            })
+            .catch(() => {
+              // 提示
+              this.$message.error('手机或者验证码错误')
+            })
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
@@ -20,7 +88,7 @@ export default {}
   background: url(../../assets/images/login_bg.jpg) no-repeat center/cover;
   .login-box {
     width: 400px;
-    height: 300px;
+    height: 330px;
     position: absolute;
     left: 50%;
     top: 50%;
